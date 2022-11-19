dc836d3f806cf1766bb5dc776c45f62439fe3627
Teeradon <teeradon.taen@bumail.net>
2022-11-19 21:33:34 +0700

    LAB1

---
+++ LABEX2.py
@@ -0,0 +1,8 @@
+h = eval(input("Enter diamond's height: "))
+k = input("Enter diamond's height: ")
+
+for j in range(k):
+    for x in range(h):
+        print(" " * (h - x), "*" * (2 * x + 1))
+    for x in range(h - 2, -1, -1):
+        print(" " * (h - x), "*" * (2 * x + 1))
